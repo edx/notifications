5fb4e12e74925398d24c6aede9d20c0348a66f2f
Sreecharan Sankaranarayanan <Sree@dhcp-18-189-15-68.dyn.MIT.EDU>
2016-10-04 13:55:18 -0400

    Replaced direct calls to enter_timestamp and exit_timestamp with calls to onEnter() and onExit() instead.

--- NotificationsPipeline.py
+++ NotificationsPipeline.py
@@ -27,10 +27,10 @@ class NotificationsPipeline(object):
         # Identify first time
         if message.current_step == pipeline_step_list[0]:
             message.history = [nm.NotificationsPipelineHistory() for i in range(len(pipeline_step_list) - 1)] # Initialize list of history objects
-            message.history[0].enter_timestamp = pytz.utc.localize(datetime.utcnow()) # Assign the entry timestamp
+            message.history[0].onEnter() # Assign the entry timestamp
             message.current_step = pipeline_step_list[1] # Update the current step
             NotificationsPipeline.str2Class(message.current_step,message.current_step.split(".")[-1]).process_message(message) # Current step processes the message
-            message.history[0].exit_timestamp = pytz.utc.localize(datetime.utcnow())
+            message.history[0].onExit()
             # exit()
             NotificationsPipeline.on_enter(message)
         # Identify last time
@@ -38,9 +38,9 @@ class NotificationsPipeline(object):
             print message.name + " " + str(message.id) + " has been delivered."
         else:
             pipeline_step_pointer = pipeline_step_list.index(message.current_step)
-            message.history[pipeline_step_pointer].enter_timestamp = pytz.utc.localize(datetime.utcnow())
+            message.history[pipeline_step_pointer].onEnter()
             pipeline_step_pointer += 1
             message.current_step = pipeline_step_list[pipeline_step_pointer] # Update the current step
             NotificationsPipeline.str2Class(pipeline_step_list[pipeline_step_pointer],pipeline_step_list[pipeline_step_pointer].split(".")[-1]).process_message(message) # Current step processes the message
-            message.history[pipeline_step_pointer-1].exit_timestamp = pytz.utc.localize(datetime.utcnow())
+            message.history[pipeline_step_pointer-1].onExit()
             NotificationsPipeline.on_enter(message)
