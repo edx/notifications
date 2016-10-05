0c5f24181987cc7aff6a7197185dba163f803c06
Sreecharan Sankaranarayanan <Sree@dhcp-18-189-15-68.dyn.MIT.EDU>
2016-10-05 12:34:31 -0400

    Replaced assetEqual with assertIsInstance to account for inheritance

--- TestNotificatonsPipeline.py
+++ TestNotificatonsPipeline.py
@@ -12,19 +12,20 @@ class TestNotificationsPipeline(unittest.TestCase):
 
     def test_composer(self):
         message = nm.NotificationsMessage()
-        self.assertEqual(type(message.id),type(uuid.uuid4()))
+        self.assertIsInstance(message.id, uuid.UUID)
 
     def test_complete_pipeline(self):
         message = nm.NotificationsMessage()
-        np.NotificationsPipeline.on_enter(message)
+        np_object = np.NotificationsPipeline()
+        np_object.on_enter(message)
         # Check the vars of the message.
-        self.assertEqual(type(message.id),type(uuid.uuid4()))
-        self.assertEqual(len(message.history),len(np.NotificationsPipeline.pipeline_step_list) - 1)
+        self.assertIsInstance(message.id, uuid.UUID)
+        self.assertEqual(len(message.history),len(np_object.pipeline_step_list) - 1)
         history_list = message.history
         for i in history_list:
-            self.assertEqual(type(i),type( nm.NotificationsPipelineHistory() ) )
-            self.assertEqual(type(i.enter_timestamp),type(pytz.utc.localize(datetime.utcnow())))
-            self.assertEqual(type(i.exit_timestamp),type(pytz.utc.localize(datetime.utcnow())))
-        self.assertEqual(message.current_step,np.NotificationsPipeline.pipeline_step_list[-1])
+            self.assertIsInstance(i,nm.NotificationsPipelineHistory)
+            self.assertEqual(type(i.enter_timestamp), type(pytz.utc.localize(datetime.utcnow())) )
+            self.assertEqual(type(i.exit_timestamp), type(pytz.utc.localize(datetime.utcnow())) )
+            self.assertEqual(message.current_step,np_object.pipeline_step_list[-1])
 if __name__ == '__main__':
     unittest.main()
