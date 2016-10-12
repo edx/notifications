d760f81b536b40da46b72c9d9db4d5a24b4ab2f7
Sreecharan Sankaranarayanan <Sree@dhcp-18-189-104-71.dyn.MIT.EDU>
2016-10-12 10:11:54 -0400

    Updated test cases

--- test_pipeline.py
+++ test_pipeline.py
@@ -1,29 +1,25 @@
-import unittest
-import sys
-import os
-sys.path.append(os.path.abspath('..'))
-import NotificationsMessage as nm
-import NotificationsPipeline as np
-import uuid
+import unittest, pytz
+from message import NotificationsMessage, PipelineHistory
+from pipeline_manager import Pipeline
+from uuid import UUID
 from datetime import datetime
-import pytz
 
-class TestNotificationsPipeline(unittest.TestCase):
+class TestPipeline(unittest.TestCase):
 
     def test_composer(self):
-        message = nm.NotificationsMessage()
-        self.assertIsInstance(message.id, uuid.UUID)
+        message = NotificationsMessage("edx.notifications.forums.post.created")
+        self.assertIsInstance(message.id, UUID)
 
     def test_complete_pipeline(self):
-        message = nm.NotificationsMessage()
-        np_object = np.NotificationsPipeline()
-        np_object.on_enter(message)
+        message = NotificationsMessage("edx.notifications.forums.post.created")
+        np_object = Pipeline()
+        np_object.pipeline_process(message)
         # Check the vars of the message.
-        self.assertIsInstance(message.id, uuid.UUID)
+        self.assertIsInstance(message.id, UUID)
         self.assertEqual( len(message.history), (len(np_object.pipeline_step_list)-1)*2 )
         history_list = message.history
         for i in history_list:
-            self.assertIsInstance(i,nm.NotificationsPipelineHistory)
+            self.assertIsInstance(i,PipelineHistory)
             self.assertEqual(type(i.timestamp), type(pytz.utc.localize(datetime.utcnow())) )
             # self.assertEqual(message.current_step,np_object.pipeline_step_list[-1]) # Testing if there are two instance for each pipeline step
 if __name__ == '__main__':
