e6244557886733607fde3cef8a0be5b2fee77b72
Sreecharan Sankaranarayanan <sree@cmu.edu>
2016-10-20 10:23:43 -0400

    Added tests for logging using mock

--- notifications/test_pipeline.py
+++ notifications/test_pipeline.py
@@ -6,6 +6,8 @@ from datetime import datetime
 import unittest
 from uuid import UUID
 
+from mock import patch
+
 from .message import NotificationsMessage, PipelineHistory
 from .pipeline_manager import Pipeline
 
@@ -15,11 +17,8 @@ class TestPipeline(unittest.TestCase):
     Implements the unit tests for the notifications pipeline.
     """
 
-    def test_composer(self):
-        message = NotificationsMessage("edx.notifications.forums.post.created")
-        self.assertIsInstance(message.uuid, UUID)
-
-    def test_complete_pipeline(self):
+    @patch('notifications.pipeline_steps.base.logging')
+    def test_complete_pipeline(self, mock_logging):
         message = NotificationsMessage("edx.notifications.forums.post.created")
         pipeline = Pipeline()
         pipeline.process(message)
@@ -30,5 +29,9 @@ class TestPipeline(unittest.TestCase):
         for i in history_list:
             self.assertIsInstance(i, PipelineHistory)
             self.assertIsInstance(i.timestamp, datetime)
+        # Check logging
+        self.assertEqual(len(mock_logging.info.call_args_list), 8)  # Successful delivery means 8 calls to logging info
+        self.assertEqual(len(mock_logging.warning.call_args_list), 0)
+
 if __name__ == '__main__':
     unittest.main()
